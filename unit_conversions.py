c8feb2dd3c8a025ad3875b777245491a32b5b167
phoebewang59 <fw4299@my.utexas.edu>
2023-10-06 16:02:30 -0500

    module for unit conversions

---
+++ unit_conversions.py
@@ -0,0 +1,29 @@
+def miles_to_km(num):
+    km = num * 1.60934
+    return km
+
+
+def km_to_miles(num):
+    miles = num/1.60934
+    return miles
+
+
+def F_to_C(num): #fahrenheit to celsius
+    F = (num*(9/5)) + 32
+    return F
+
+
+def C_to_F(num):#celsius to fahrenheit
+    C = (num - 32)*(5/9)
+    return C
+
+
+def kilo_to_pound(num):
+    pound = num*2.205
+    return pound
+
+
+def pound_to_kilo(num):
+    kilogram = num/2.205
+    return kilogram
+
